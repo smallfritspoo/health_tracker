edfd4cd40570af3c7d96bb78af27ff96f29033cf
smallfritspoo <richardson.chesney@gmail.com>
2019-12-18 21:51:14 -0600

    more stuff

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -44,6 +45,53 @@ class Weight(db.Model):
 
     def __repr__(self) -> repr:
         return f"<Weight {self.weight}>"
+
+
+class NutritionInformation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    report_id = db.Column(db.Integer, unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    meal_type = db.Column(db.)
+    calories = db.Column(db.Integer)
+    calories_from_fat = db.Column(db.Integer)
+    total_fat = db.Column(db.Integer)
+    trans_fat = db.Column(db.Integer)
+    saturated_fat = db.Column(db.Integer)
+    cholesterol = db.Column(db.Integer)
+    sodium = db.Column(db.Integer)
+    total_carbohydrate = db.Column(db.Integer)
+    dietary_fiber = db.Column(db.Integer)
+    sugars = db.Column(db.Integer)
+    protein = db.Column(db.Integer)
+    vitamin_a = db.Column(db.Integer)
+    vitamin_c = db.Column(db.Integer)
+    calcium = db.Column(db.Integer)
+    iron = db.Column(db.Integer)
+    thiamine = db.Column(db.Integer)
+    riboflavin = db.Column(db.Integer)
+    niacin = db.Column(db.Integer)
+    pantothenic_acid = db.Column(db.Integer)
+    vitamin_b6 = db.Column(db.Integer)
+    folate = db.Column(db.Integer)
+    biotin = db.Column(db.Integer)
+    vitamin_b12 = db.Column(db.Integer)
+    vitamin_d = db.Column(db.Integer)
+    vitamin_e = db.Column(db.Integer)
+    vitamin_k = db.Column(db.Integer)
+    phosphorus = db.Column(db.Integer)
+    iodine = db.Column(db.Integer)
+    magnesium = db.Column(db.Integer)
+    zinc = db.Column(db.Integer)
+    selenium = db.Column(db.Integer)
+    copper = db.Column(db.Integer)
+    manganese = db.Column(db.Integer)
+    chromium = db.Column(db.Integer)
+    molybdenum = db.Column(db.Integer)
+    chloride = db.Column(db.Integer)
+
+    def __repr__(self) -> repr:
+        return f"<Calories {self.calories}>"
 
 
 @login.user_loader
